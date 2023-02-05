acdf311256e0f991320235fdfbbee168cb2b6461
jatlnas <johannes.altnas@protonmail.com>
2023-02-05 19:46:35 +0100

    Cleaning session

--- Skill_extraction.R
+++ Skill_extraction.R
@@ -2,9 +2,7 @@
 ##################################
 library(tidyverse)               #
 library(dplyr)                   #
-#library(rnaturalearth)           #
 library(ggmap)                   #
-#library(sf)                      #
 library(maps)                    #
 ##################################
 
@@ -31,7 +29,7 @@ remove = which(grepl("utvecklare",title_data) |
                  !grepl("ai",(title_data))) #or ai
 my_data = my_data[-remove,]
 
-#Corrections for wrongly assigned types based on title
+#Corrections for wrongly assigned job-types based on title
 for(i in 1:nrow(my_data)){
   if(tolower(my_data[i,1]) == "data engineer"){
     my_data[i,7] = "data engineer" }
@@ -43,6 +41,7 @@ for(i in 1:nrow(my_data)){
 
 get_top_skills <- function(skills_data){
   
+  #Hard-coding skills
   skills_list = c("python","r", "apache", "spark", "sql", "pandas", 
                   "numpy", "ggplot", "sklear", "dplyr", "mysql", "mongodb",
                   "seaborn","tableau","qlik", "julia","matlab","qlikview",
@@ -51,36 +50,35 @@ get_top_skills <- function(skills_data){
                   "linux", "google","cloud", "dashboard", "visualization","dbt", 
                   "bigquery", "git", "datawarehouse")
   
-  desc_data = tolower(skills_data[,6]) 
-  desc_data = gsub("\n", " ",desc_data)
-  desc_data = str_replace_all(desc_data, "[^[:alnum:]]", " ")
+  #Clean description data
+  desc_data = tolower(skills_data[,6]) #force lower case
+  desc_data = gsub("\n", " ",desc_data) #\n is new row for descriptions
+  desc_data = str_replace_all(desc_data, "[^[:alnum:]]", " ") #remove anything not letters or numbers
   
-  word_lists = (str_split(desc_data, " ", simplify = FALSE))
-  unique_word_lists = sapply(word_lists, function(z) unique(z))
+  word_lists = (str_split(desc_data, " ", simplify = FALSE)) #split desc into words
+  unique_word_lists = sapply(word_lists, function(z) unique(z)) #keep only unique words
 
-  full_word_list = do.call(c, unique_word_lists)
+  full_word_list = do.call(c, unique_word_lists) #Combine unique lists from all descs.
   
+  #Tabulate number of times skills appear in full word list
   word_count <- as.data.frame(table(full_word_list[which(full_word_list %in% skills_list)]))
   
+  #Add occurences of "power bi"
   if("powerbi" %in% word_count$Var1){
     word_count[word_count$Var1 == "powerbi",2] = 
              word_count[word_count$Var1 == "powerbi",2] + 
              sum(grepl("power bi",desc_data))
   }
+  #Add occurences of "data bricks"
   if("databricks" %in% word_count$Var1){
     word_count[word_count$Var1 == "databricks",2] = 
     word_count[word_count$Var1 == "databricks",2] + 
     sum(grepl("data bricks",desc_data))
   }
-  if("spark" %in% word_count$Var1 & "apache" %in% word_count$Var1){
-    word_count[word_count$Var1 == "spark",2] = 
-    word_count[word_count$Var1 == "spark",2] +
-    word_count[word_count$Var1 == "apache",2] - 
-    sum(grepl("apache spark",desc_data))
-  }
   
-  word_count <- word_count %>% arrange(desc(word_count[,2]))
-  word_count[,1] = str_to_title(word_count[,1])
+  #Format list
+  word_count <- word_count %>% arrange(desc(word_count[,2])) #Descending order
+  word_count[,1] = str_to_title(word_count[,1]) #Capitalize first letter
   word_count[which(word_count[,1] =="Sql"),1] = "SQL"
   word_count[which(word_count[,1] =="Aws"),1] = "AWS"
   
@@ -88,10 +86,9 @@ get_top_skills <- function(skills_data){
     word_count[which(word_count[,1] =="Powerbi"),1] = "PowerBI"
   }
   
-  word_count[,2] = (word_count[,2]/dim(skills_data)[1])*100
+  word_count[,2] = (word_count[,2]/dim(skills_data)[1])*100 #Into %'s
   return(word_count)
 }
-
 
 plot_top_7 <- function(word_count, colour, title, n){
   word_topten = word_count[1:7,]
@@ -111,13 +108,17 @@ plot_top_7 <- function(word_count, colour, title, n){
     theme(axis.title=element_text(size=12,face="bold"), legend.position = "none")
 }
 
+#Create one dataframe for each job title
 ds_data = my_data %>% filter(Type == "data scientist" | Type =="machine learning engineer")
 da_data = my_data %>% filter(Type == "data analyst")
 de_data = my_data %>% filter(Type == "data engineer")
 mle_data = my_data %>% filter(Type == "machine learning engineer")
+
+#Create one dataframe general fields "data science" and "data analytics"
 gen_ds_data = my_data %>% filter(Type == "data science" | Type == "data scientist")
 gen_da_data = my_data %>% filter(Type == "data analytics" | Type == "data analyst")
 
+#Retrieve and plot top skills for all groups 
 ds_skills = get_top_skills(ds_data)
 plot_top_7(ds_skills,"Blues", "Data Scientist",nrow(ds_data))
 
@@ -135,11 +136,14 @@ plot_top_7(all_skills,"Reds", "All postings",nrow(my_data))
 
 gen_ds_skills =get_top_skills(gen_ds_data)
 plot_top_7(gen_ds_skills,"Blues", "Data Science",nrow(gen_ds_data))
+
 gen_da_skills = get_top_skills(gen_da_data)
 plot_top_7(gen_da_skills,"Greens", "Data Analytics",nrow(gen_da_data))
 
+#Count company occurances
+comp_count = my_data %>% group_by(Company) %>% summarise(n=n())
 
-##Creating job-location map##
+##### Creating job-location map #####
 
 
 #Cleaning dirty location data, order by number of posts
@@ -162,9 +166,9 @@ loc_count = loc %>% group_by(loc) %>% summarise(n=n())
 loc_count = loc_count[order(loc_count$n,decreasing=TRUE),]
 
 
-SE <- map_data("world") %>% filter(region=="Sweden")
+SE <- map_data("world") %>% filter(region=="Sweden") #Load Sweden map-data
 data <- read.csv("se.csv") #from https://simplemaps.com/data/se-cities
-data <- merge(loc_count,data,by.x = "loc", by.y = "city")
+data <- merge(loc_count,data,by.x = "loc", by.y = "city") #Tabulate count per city
 
 data %>%
   arrange(n) %>% 
